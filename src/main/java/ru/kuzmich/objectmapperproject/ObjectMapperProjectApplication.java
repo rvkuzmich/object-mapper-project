c0f50f571c28d9ef2d14e4f6ce80db8d2b5c5a56
Roman Kuzmich <rvku2mich@yandex.ru>
2026-02-22 10:20:58 +0300

    initial commit

---
+++ src/main/java/ru/kuzmich/objectmapperproject/ObjectMapperProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.kuzmich.objectmapperproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectMapperProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectMapperProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kuzmich/objectmapperproject/ObjectMapperProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kuzmich.objectmapperproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectMapperProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
